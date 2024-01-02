2dd12ea1fb29d96cedb1d3a0fa4df8fc79b0d50b
Viraj Sharma <viraj19r@gmail.com>
2024-01-03 00:03:54 +0530

    change config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
 
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    basePath: isProd ? '/viraj19r': '',
     output: 'export',
     images: {
         unoptimized: true,
