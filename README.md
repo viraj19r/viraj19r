9a797ccff266707d2a76240c5103060616aaaff3
Viraj Sharma <viraj19r@gmail.com>
2024-01-03 09:38:24 +0530

    update navigation bar

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
-- 👋 Hi, I’m Viraj Sharma
-- 👀 I’m interested in learning new skills
-- 🌱 I’m currently learning skills related to Data Science
-- 💞️ I’m looking to collaborate on open source projects
-- 📫 Reach or connect with me on [LinkedIn](https://www.linkedin.com/in/virajsharma/)
 
-<!---
-viraj19r/viraj19r is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,19 +11,20 @@ const Header: React.FC = () => {
         </span>
       </div>
       <nav className="ml-auto flex gap-2 sm:gap-4 lg:gap-6 md:ml-auto md:justify-start justify-center w-full md:w-auto">
-        <Link href="#about" passHref>
-          <span className="text-sm md:text-lg font-medium text-gray-600 hover:text-white hover:bg-blue-700 hover:border-b-2 hover:border-blue-500 transition-all duration-500 ease-in-out px-4 py-2 rounded-md cursor-pointer">
-            About
-          </span>
-        </Link>
+        
         <Link href="#skills" passHref>
           <span className="text-sm md:text-lg font-medium text-gray-600 hover:text-white hover:bg-blue-700 hover:border-b-2 hover:border-blue-500 transition-all duration-500 ease-in-out px-4 py-2 rounded-md cursor-pointer">
             Skills
           </span>
         </Link>
-        <Link href="/projects" passHref>
+        <Link href="#projects" passHref>
           <span className="text-sm md:text-lg font-medium text-gray-600 hover:text-white hover:bg-blue-700 hover:border-b-2 hover:border-blue-500 transition-all duration-500 ease-in-out px-4 py-2 rounded-md cursor-pointer">
             Projects
+          </span>
+        </Link>
+        <Link href="#resume" passHref>
+          <span className="text-sm md:text-lg font-medium text-gray-600 hover:text-white hover:bg-blue-700 hover:border-b-2 hover:border-blue-500 transition-all duration-500 ease-in-out px-4 py-2 rounded-md cursor-pointer">
+            Education
           </span>
         </Link>
         <Link href="#contact" passHref>
@@ -31,6 +32,7 @@ const Header: React.FC = () => {
             Contact
           </span>
         </Link>
+        
       </nav>
     </header>
   );
